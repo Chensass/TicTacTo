70dfb94ec318d25d16a59a27ce987438e07f65c7
Chen Sasson <cheansa312@gmail.com>
2022-04-01 18:25:53 +0300

    feat: created main.py and venv

---
+++ main.py
@@ -0,0 +1,24 @@
+
+# Librarys and things to learn:
+# backend- multipule client sockets
+# fron end - learn pygame
+
+# List of feature:
+# board - create a class of the board tha
+# client
+# server to connect between clients
+
+# Sceduale:
+#part 1
+# create a repository in git and clone project
+# create venv
+
+#part 2
+# learn multithreading
+# create multipule client server connections - pass a string to print
+
+# part 3
+# learen pygame
+# create the board and display
+# connect between clients , board and server
+# create readme and requierments
